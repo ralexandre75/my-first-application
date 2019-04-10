2d12cb00d46b62effe0e694717c30e1b0f465307
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-04-10 16:10:11 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <Text>Peggy !</Text>
+    );
+  }
+}
+

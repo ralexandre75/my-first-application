772d63a6a531771801ec7abe43e15a9a0ebc3b5d
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-04-10 19:13:52 +0200

    positioning

--- components/header/index.js
+++ components/header/index.js
@@ -5,8 +5,8 @@ import { style } from './style';
 const Header = ({ content }) => (
   <View>
     <View style={style.subHeader} />
-    <View>
-      <Text> {content} </Text>
+    <View style={style.header}>
+      <Text style={style.text}> {content} </Text>
     </View>
   </View>
 );
--- components/header/style.js
+++ components/header/style.js
@@ -2,8 +2,21 @@ import { StyleSheet } from 'react-native';
 import { APP_COLORS } from '../../styles/color';
 
 export const style = StyleSheet.create({
-    subHeader: {
-        backgroundColor: APP_COLORS.darkPrimary,
-        height: 30
-    }
-});
+         subHeader: {
+           backgroundColor: APP_COLORS.darkPrimary,
+           height: 30
+         },
+         header: {
+           backgroundColor: APP_COLORS.primary,
+           height: 150,
+           justifyContent: 'center',
+           alignItems: 'center',
+           shadowColor: APP_COLORS.shadow,
+           shadowOpacity: 0.2,
+           shadowOffset: { height: 10 }     
+         },
+         text: {
+           color: APP_COLORS.primaryText,
+           fontSize: 30
+         }
+       });

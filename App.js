f9f70de61066da283a917e6346a0bd37295404cc
ralexandre75 <rene.alexandre@grimoire-design.fr>
2019-04-11 11:30:03 +0200

    library React native elements use

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
+import { View } from 'react-native';
+import { Button } from 'react-native-elements';
 import React from 'react';
 import Header from './components/header';
 
+
 export default class App extends React.Component {
+
   render() {
     return (
+      <View>
         <Header content="Liste des tâches" />
+        <Button 
+          large
+          iconRight
+          icon={{ name: 'code' }}
+          title="pomme"
+        />
+      </View>
     );
   }
 }
